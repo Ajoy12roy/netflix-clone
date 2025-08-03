38b1503f7492c4c6f9d088c2173a029877fb9541
ajoy12roy <abjoyroy@gmail.com>
2025-08-04 01:12:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HOME from './pages/Home/Home'
+
+const App = () => {
+  return (
+    <div>
+      <HOME />
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className= 'navbar' >
+        tumi chodao
+    </div>
+
+    
+  )
+}
+
+export default Navbar
