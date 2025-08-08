4a8516ded35bc3385729bb5c5573673d8fe0538c
ajoy12roy <abjoyroy@gmail.com>
2025-08-08 23:45:04 +0600

    total nav donex

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -30,7 +30,11 @@ const Navbar = () => {
         <div className='navbar-profile'> 
              <img src={profile_img} alt="" className='profile' />
                  <img src={caret_icon} alt="" />
+                 <div className="dropdown">
+                  <p>sign out of netflix </p>
+                 </div>
         </div>
+
 
         
       </div>
