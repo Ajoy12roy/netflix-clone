eab6a493542354f72e053d5ffa490fef28df0afe
ajoy12roy <abjoyroy@gmail.com>
2025-08-08 21:25:50 +0600

    total nav flex

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -10,7 +10,7 @@ import caret_icon from '../../assets/caret_icon.svg'
 const Navbar = () => {
   return (
     <div className= 'navbar' >
-      <div className='Navbar-left'>
+      <div className='navbar-left'>
         <img src={logo} alt=''  />
         <ul>
           <li>Home</li>
@@ -20,13 +20,14 @@ const Navbar = () => {
           <li>search by language</li>
         </ul>
       </div>
-      <div className='Navbar-right'>
+      
+      <div className='navbar-right'>
 
         <img src={search_icon} alt="" className='icon' />
         <p>Children</p>
 
         <img src={bell_icon} alt="" className='icon' />
-        <div className='Navbar-profile'> 
+        <div className='navbar-profile'> 
              <img src={profile_img} alt="" className='profile' />
                  <img src={caret_icon} alt="" />
         </div>
